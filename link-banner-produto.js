cdbf3a8f1534e51385c377aaafc5a46252d3934f
miqueias9889 <miqueias9889@gmail.com>
2022-06-09 05:23:53 -0300

    economizaplay 40

--- link-banner-produto.js
+++ link-banner-produto.js
@@ -1,11 +1,11 @@
 
 function tirarEscapeXml(unsafe) {
     return unsafe
-         .replace(/&amp;/g, " ")
-         .replace(/&lt;/g, " ")
-         .replace(/&gt;/g, " ")
-         .replace(/&quot;/g, " ")
-         .replace(/&#039;/g, " ");
+         .replace(/&amp\;/g, " ")
+         .replace(/&lt\;/g, " ")
+         .replace(/&gt\;/g, " ")
+         .replace(/&quot\;/g, " ")
+         .replace(/&#039\;/g, " ");
  }       
        
 var mytxturls = tirarEscapeXml(txtfims);
